822865a2d26b432c4f42520305a7e3ff038e0fc9
Yading <songyading@gmail.com>
2014-04-17 23:48:28 +0100

    Add a new lines from week2 on R functions

--- week2.R
+++ week2.R
@@ -22,3 +22,58 @@ columnmean <- function(x, removeNA = TRUE){
 
     means 
 }
+
+
+make.power <- function(n){
+    pow <- function(x){
+        x^n
+    }
+    pow 
+}
+
+y <- 10
+f <- function(x){
+    y <- 2
+    y^2 + g(x)
+}
+
+g<- function(x){
+    x*y
+}
+
+#Date class
+x <- as.Date("1970-01-01")
+unclass(x)
+x <- as.Date("1970-01-02")
+unclass(x)
+
+#POSIXct 
+#POSIXlt 
+#weekdays 
+#months 
+#quarters "Q1","Q2","Q3","Q4"
+
+x <- Sys.time()
+p <- as.POSIXlt(x)
+names(unclass(p))
+p$sec
+
+
+x <- Sys.time()
+uncless(x)
+
+datestring <- c("January 10, 2012 10:40", "December 9, 2011")
+x <- strptime(datestring, "%B %d, %Y %H:%M")
+
+class(x)
+
+
+x <- as.Date("2012-03-01")
+y <- as.Date("2012-02-28")
+
+x - y 
+
+x <- as.POSIXct("2012-10-25 01:00:00")
+y <- as.POSIXct("2012-10-25 06:00:00", tz ="GMT")
+
+y-x
